514c364fb9dd569c807de4f444d2e6d8f340d367
John Leeds <leedsjohn6@gmail.com>
2022-05-07 15:26:45 -0700

    Added some colors

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,7 @@
 def main():
 	print("Hello! This is the first thing.")
 	print("I'm working on some cool new stuff!")
+	print("here's some colors: red, green, blue")
 
 if __name__ == "__main__":
 	main()
