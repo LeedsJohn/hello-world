f796d60f24151dc3c6c7bf242b34d75549b8e361
John Leeds <leedsjohn6@gmail.com>
2022-05-07 15:24:32 -0700

    found the answer to 10x12

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,7 @@
 def main():
 	print("Hello! This is the first thing.")
+	print("somebody said tehy would pay me a lot of money if I can find the answer to 10x12")
+	print(10*12)
 
 if __name__ == "__main__":
 	main()
